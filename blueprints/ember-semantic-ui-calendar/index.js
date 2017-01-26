b1db729795a8633cbe1235be6803cffbb45cfb3f
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-01-26 16:58:49 -0200

    Add missing method in default blueprint

--- blueprints/ember-semantic-ui-calendar/index.js
+++ blueprints/ember-semantic-ui-calendar/index.js
@@ -1,5 +1,9 @@
 /*jshint node:true*/
 module.exports = {
+  description: 'Install Semantic UI Calendar',
+
+  normalizeEntityName: function() {},
+
   afterInstall: function () {
     return this.addBowerPackageToProject('semantic-ui-calendar');
   }
