42c06f3e3ec10cd159256b2a59cfa41d02a0522b
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-05-24 14:58:11 -0300

    Remove ember-addon-genie

--- config/release.js
+++ config/release.js
@@ -1,23 +1,17 @@
 /* jshint node:true */
-var publishToGhPages = require('ember-addon-genie/lib/tasks/publish-to-gh-pages');
-// var RSVP = require('rsvp');
+var execSync = require('child_process').execSync;
 
 // For details on each option run `ember help release`
 module.exports = {
-  afterPush: publishToGhPages,
-  publish: true
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
+  publish: true,
+  afterPush: function(project, versions) {
+    runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
+    runCommand('git push origin gh-pages:gh-pages');
+  }
 };
+
+function runCommand(command) {
+  console.log('running: ' + command);
+  var output = execSync(command, { encoding: 'utf8' });
+  console.log(output);
+}
